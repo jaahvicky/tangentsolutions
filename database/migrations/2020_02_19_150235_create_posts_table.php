68277dbaad0f2f2f73f5c711b4ece35523939966
Victor Musvibe <jaahvicky@gmail.com>
2020-02-26 01:59:38 +0000

    update posts migration

--- database/migrations/2020_02_19_150235_create_posts_table.php
+++ database/migrations/2020_02_19_150235_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->text('description');
+            $table->text('content');
             $table->integer('user_id')->unsigned();
             $table->integer('category_id')->unsigned();
             $table->timestamps();
